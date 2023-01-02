08008c12aba2789179adaa9e7db36dc0f25b5508
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-02 12:09:32 +0600

    first iteration complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <NavBar></NavBar>
+      <div className="m-2">
+        <div className="form-control max-w-md mx-auto">
+          <input type="text" placeholder="Search" className="input input-bordered" />
+        </div>
+      </div>
+      <div className="m-2">
+
+      </div>
+      <div className="m-2 mt-8">
+        <div className="h-[180px] bg-[#D9D9D9] flex justify-center items-center mb-8 p-2">One</div>
+        <div className="h-[180px] bg-[#D9D9D9] flex justify-end p-2 mb-8">Two</div>
+        <div className="h-[180px] bg-[#D9D9D9] flex items-end p-2">Three</div>
+      </div>
     </div>
   );
 }
