1f247afd3cf4b769cc2e29401b014831e030ed7b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-03 15:35:31 +0600

    added background to task

--- src/components/CardTask/CardTask.js
+++ src/components/CardTask/CardTask.js
@@ -3,10 +3,10 @@ import './CardTask.css'
 
 const CardTask = () => {
   return (
-    <div className="custom-card">
+    <div className="custom-card bg-gray-300">
       <div className="custom-card-body">
-        <input defaultValue="College" type="text" className="input-1" />
-        <input defaultValue="Field" type="text" className="input-1" />
+        <input defaultValue="College" type="text" className="input-1 text-xs pl-2" />
+        <input defaultValue="Field" type="text" className="input-1 text-xs pl-2" />
       </div>
     </div>
   );
